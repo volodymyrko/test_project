748f297fa5b55904d5352205b46f7263990d4250
Volodymyr Kovtun <vova-zms@yandex.ua>
2012-03-16 11:16:24 +0200

    #3 add test

--- test42cc/http_middleware/tests.py
+++ test42cc/http_middleware/tests.py
@@ -7,10 +7,13 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from django.utils import unittest
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 from http_middleware.models import HttpRequestStore
 
 class HttpReqeustTest(unittest.TestCase):
     def setUp(self):
+        self.client = Client()
         self.path = '/'
         self.record = HttpRequestStore.objects.create(path=self.path,
             method='GET', remote_address='127.0.0.1')
@@ -19,3 +22,10 @@ class HttpReqeustTest(unittest.TestCase):
         """ Test for request saving in db
         """
         self.assertTrue(HttpRequestStore.objects.filter(path=self.path).count() > 0)
+
+    def test_get_response(self):
+        """ testing  get '/request' result
+        """
+        url = reverse('requests')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)

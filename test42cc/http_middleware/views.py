3962c4e3771785ac35fda5e78fa7fff353941ed3
Volodymyr Kovtun <vova-zms@yandex.ua>
2012-03-16 14:56:21 +0200

    #3 fix views//  change order select

--- test42cc/http_middleware/views.py
+++ test42cc/http_middleware/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from http_middleware.models import HttpRequestStore
 
 RECORDS_BY_PAGE = 10
@@ -9,12 +9,15 @@ RECORDS_BY_PAGE = 10
 def requests(request):
     """ show http requests stored (order_by middleware) in db
     """
-    all_request = HttpRequestStore.objects.order_by('-time')
+    all_request = HttpRequestStore.objects.all()
     page = request.GET.get('page', 1)
     paginator = Paginator(all_request, RECORDS_BY_PAGE)
     try:
         requests = paginator.page(page)
     except EmptyPage:
         requests = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        requests = paginator.page(1)
+
     return render_to_response('requests.html', {'requests': requests})
 
